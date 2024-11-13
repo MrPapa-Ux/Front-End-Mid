d49b024bf4b9a4d175322ec2118e1e16beb2d61a
MrPapa-Ux <tosans977@gmail.com>
2024-11-14 00:11:40 +0800

    make some app js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  {
-    return <div className="App"></div>;
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Custom React App!</h1>
+        <p>This is a custom design using React.</p>
+        <button onClick={() => alert("Button Clicked!")}>Click Me</button>
+      </header>
+    </div>
+  );
 }
-
 export default App;
