78bcd376e138fabfa0a0c79cc587a11063deb053
MrPapa-Ux <tosans977@gmail.com>
2024-11-14 00:26:44 +0800

    build and atur file.js supaya sesuai dengan components.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 // src/App.js
-import React, { useState } from "react";
+import React from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Achievements from "./components/Achievements";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React Counter</h1>
-        <p>Current count: {count}</p>
-        <button onClick={() => setCount(count + 1)}>Increase Count</button>
-      </header>
+      <Header />
+      <About />
+      <Skills />
+      <Achievements />
+      <Footer />
     </div>
   );
 }
