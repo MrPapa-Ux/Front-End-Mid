37df13c584162a4a07ca670f94920836fc86d3bf
MrPapa-Ux <tosans977@gmail.com>
2024-11-16 22:51:42 +0800

    test

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Achievements from "./components/Achievements";
 import Footer from "./components/Footer";
-import "./App.css"; // Impor App.css di sini, di dalam App.js
-
+import "./App.css";
 function App() {
   return (
     <div className="App">
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,29 @@
 // src/components/Header/index.js
 import React from "react";
-import "./index.css"; // Mengimpor styling khusus untuk Header
-import profileImage from "../../assets/images/1.jpg"; // Mengimpor gambar secara langsung
+import "./index.css"; // Mengimpor styling untuk Header
+import profileImage from "../../assets/images/1.jpg"; // Mengimpor gambar langsung
 
 function Header() {
+  // Data yang ingin ditampilkan di table
+  const achievementsData = [
+    { section: "Award", details: "Best Developer 2023" },
+    { section: "Certification", details: "Full Stack Developer Cert." },
+    { section: "Project", details: "Project Management App" },
+  ];
+
   return (
-    <header className="header">
-      <div className="profile-container">
-        <img
-          src={profileImage} // Menggunakan gambar yang diimpor
-          alt="Profile"
-          className="profile-image"
-        />
-      </div>
-      <div className="header-text">
-        <h1>Gerungan, Glenvidi Yosua</h1>
-        <p>Computer-Science | Informatics | Programmer-Handal</p>
-      </div>
-    </header>
+    <div>
+      <header className="header">
+        <div className="profile-container">
+          {/* Menggunakan gambar yang diimpor */}
+          <img src={profileImage} alt="Profile" className="profile-image" />
+        </div>
+        <div className="header-text">
+          <h1>Gerungan, Glenvidi Yosua</h1>
+          <p>Computer-Science | Informatics | Programmer-Handal</p>
+        </div>
+      </header>
+    </div>
   );
 }
 
