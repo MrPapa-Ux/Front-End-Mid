148c66a52f85486412cb2d0762a11f2e562709ae
MrPapa-Ux <tosans977@gmail.com>
2024-11-16 23:18:50 +0800

    im stress

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,14 +1,19 @@
-// src/components/About.js
+// src/components/About/index.js
 import React from "react";
+import "./index.css"; // Mengimpor styling khusus untuk About
 
 function About() {
   return (
     <section className="about">
-      <h2>About Me</h2>
-      <p>
-        I am a passionate developer with experience in creating dynamic web
-        applications...
-      </p>
+      <div className="about-text">
+        <h2>About Me</h2>
+        <p>
+          I am a passionate developer with experience in creating dynamic web
+          applications. I specialize in front-end development, but I am also
+          familiar with full-stack technologies. I strive to write clean and
+          efficient code, always learning and improving my skills.
+        </p>
+      </div>
     </section>
   );
 }
