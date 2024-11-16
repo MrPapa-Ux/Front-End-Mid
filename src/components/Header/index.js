5bcc387cb561283dc1a75eb55fb5ed6b712bc7a8
MrPapa-Ux <tosans977@gmail.com>
2024-11-16 13:58:09 +0800

    Edit Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,21 @@
 // src/components/Header/index.js
 import React from "react";
 import "./index.css"; // Mengimpor styling khusus untuk Header
+import profileImage from "../../assets/images/1.jpg"; // Mengimpor gambar secara langsung
 
 function Header() {
   return (
     <header className="header">
       <div className="profile-container">
         <img
-          src={process.env.PUBLIC_URL + "/assets/images/1.jpg"}
+          src={profileImage} // Menggunakan gambar yang diimpor
           alt="Profile"
           className="profile-image"
         />
       </div>
       <div className="header-text">
-        <h1>Your Name</h1>
-        <p>Web Developer | Designer | Programmer</p>
+        <h1>Gerungan, Glenvidi Yosua</h1>
+        <p>Computer-Science | Informatics | Programmer-Handal</p>
       </div>
     </header>
   );
