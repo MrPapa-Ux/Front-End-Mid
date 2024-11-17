63f0a6266967dc6aed9222cb5187da1512d86b80
MrPapa-Ux <tosans977@gmail.com>
2024-11-17 08:34:17 +0800

    edit Header for at least 1 hour

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,18 +4,18 @@ import "./index.css"; // Mengimpor styling untuk Header
 import profileImage from "../../assets/images/1.jpg"; // Mengimpor gambar langsung
 
 function Header() {
-  // Data yang ingin ditampilkan di table
-  const achievementsData = [
-    { section: "Award", details: "Best Developer 2023" },
-    { section: "Certification", details: "Full Stack Developer Cert." },
-    { section: "Project", details: "Project Management App" },
-  ];
-
   return (
     <div>
       <header className="header">
+        {/* Menambahkan video latar belakang */}
+        <video autoPlay muted loop>
+          <source
+            src="/videos/Five Nights at Freddy's 2 Song - The Living Tombstone (FNAF2).mp4"
+            type="video/mp4"
+          />
+        </video>
+
         <div className="profile-container">
-          {/* Menggunakan gambar yang diimpor */}
           <img src={profileImage} alt="Profile" className="profile-image" />
         </div>
         <div className="header-text">
