3d5676efa358bd1b9072c0cd2841b8be807f6d12
MrPapa-Ux <tosans977@gmail.com>
2024-11-17 10:50:28 +0800

    insert photo slider

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,49 @@
-// src/components/Header/index.js
 import React from "react";
-import "./index.css"; // Mengimpor styling untuk Header
-import profileImage from "../../assets/images/1.jpg"; // Mengimpor gambar langsung
+import Slider from "react-slick";
+import "./index.css";
+import profileImage from "../../assets/images/1.jpg"; // Gambar profil yang digunakan di header
+
+// Menambahkan gambar untuk slider
+const images = [
+  "fox.png", // Ganti dengan path gambar PNG Anda
+  "oldman.png",
+  "wolf.png",
+];
 
 function Header() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
+
   return (
     <div>
       <header className="header">
-        {/* Menambahkan video latar belakang */}
-        <video autoPlay muted loop>
-          <source
-            src="/videos/Five Nights at Freddy's 2 Song - The Living Tombstone (FNAF2).mp4 (1)"
-            type="video/mp4"
-          />
-        </video>
+        {/* Slider gambar PNG */}
+        <Slider {...settings}>
+          {images.map((image, index) => (
+            <div key={index}>
+              <img
+                src={image}
+                alt={`slide-${index}`}
+                className="slider-image"
+              />
+            </div>
+          ))}
+        </Slider>
 
+        {/* Profil dan teks header */}
         <div className="profile-container">
           <img src={profileImage} alt="Profile" className="profile-image" />
         </div>
         <div className="header-text">
           <h1>Gerungan, Glenvidi Yosua</h1>
-          <p>Computer-Science | Informatics | Programmer-Handal</p>
+          <p>Computer-Science | Informatics | Newbie-Designer</p>
         </div>
       </header>
     </div>
