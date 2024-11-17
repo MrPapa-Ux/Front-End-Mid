82bc9eba23b17c659a6ef4dbcd14d6e5ebc9719f
MrPapa-Ux <tosans977@gmail.com>
2024-11-17 14:11:58 +0800

    I make all to normal

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,43 +1,14 @@
+// src/components/Header/index.js
 import React from "react";
-import Slider from "react-slick";
 import "./index.css";
-import profileImage from "../../assets/images/1.jpg"; // Gambar profil yang digunakan di header
-
-// Menambahkan gambar untuk slider
-const images = [
-  "fox.png", // Ganti dengan path gambar PNG Anda
-  "oldman.png",
-  "wolf.png",
-];
+import profileImage from "../../assets/images/1.jpg";
 
 function Header() {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  };
-
   return (
     <div>
       <header className="header">
-        {/* Slider gambar PNG */}
-        <Slider {...settings}>
-          {images.map((image, index) => (
-            <div key={index}>
-              <img
-                src={image}
-                alt={`slide-${index}`}
-                className="slider-image"
-              />
-            </div>
-          ))}
-        </Slider>
-
-        {/* Profil dan teks header */}
+        {" "}
+        {/* Menambahkan penutupan tag header */}
         <div className="profile-container">
           <img src={profileImage} alt="Profile" className="profile-image" />
         </div>
@@ -45,7 +16,8 @@ function Header() {
           <h1>Gerungan, Glenvidi Yosua</h1>
           <p>Computer-Science | Informatics | Newbie-Designer</p>
         </div>
-      </header>
+      </header>{" "}
+      {/* Menutup tag header */}
     </div>
   );
 }
